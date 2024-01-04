25f1060c42738c3f8c3e3fceb3db1de64ad72cdc
Alexandra Osennyaya <alexosenn@gmail.com>
2024-01-04 10:56:46 +0300

    Updated tests on android

--- tests/android/test_wikipedia.py
+++ tests/android/test_wikipedia.py
@@ -3,7 +3,7 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selene import browser, have
 
 
-def test_search(android_mobile_management):
+def test_search_appium(android_mobile_management):
     with step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
@@ -14,11 +14,12 @@ def test_search(android_mobile_management):
         results.first.should(have.text('Appium'))
 
 
-def test_open_article(android_mobile_management):
-    with step('Search article'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Appium')
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).click()
+def test_search_jenkins(android_mobile_management):
+    with step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Jenkins')
 
-    with step('Verify article title'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Appium'))
+    with step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Jenkins'))
