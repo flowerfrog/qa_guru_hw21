54645502d6a09d3c5b7e26bbb2bf5f76ab66f736
Alexandra Osennyaya <alexosenn@gmail.com>
2024-01-04 10:17:52 +0300

    Added test android

---
+++ tests/android/test_wikipedia.py
@@ -0,0 +1,24 @@
+from allure_commons._allure import step
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+
+def test_search(android_mobile_management):
+    with step('Type search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
+
+    with step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Appium'))
+
+
+def test_open_article(android_mobile_management):
+    with step('Search article'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Appium')
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).click()
+
+    with step('Verify article title'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Appium'))
